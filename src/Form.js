e3c44daefe5de4ea88680a72ca11311c693ca446
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-24 16:33:52 -0500

    still WIP; throwing an error about missing closing tag, but the tags are all there.

--- src/Form.js
+++ src/Form.js
@@ -1,35 +1,73 @@
-import React from 'react';
-
+import React, { useState } from "react";
 
 export default function Form() {
+  const [member, setMember] = useState({
+    username: "",
+    email: "",
+    password: ""
+  });
 
-    return(
-    <form>
-      <label>
-        Username: 
-        <input 
-          type='name'
-          placeholder="name"/>
-      </label>
-      
-      <label>
-        E-Mail:
-        <input 
-        type='email'
-        placeholder="email"/>
-      </label>
-      
-      <label>
-        Password:
-        <input 
-        type='password'
-        placeholder="password"
-        />
-      </label>
+  function handleChange(event) {
+    const updateMember = { ...member, [event.target.name]: event.target.value };
+
+    setMember(updateMember);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Add to List:</legend>
+        <div className="form-group row">
+          <label for="username" className="col-sm-2 col-form-label">
+            Username
+            <div className="col-sm-10">
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                placeholder="Choose a Username"
+                value={member.username}
+                onChange={handleChange}
+              />
+            </div>
+          </label>
+        </div>
+        <div className="form-group">
+          <label for="email">Email</label>
+
+          <div className="col-sm-10">
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              placeholder="Enter your mail address"
+              value={member.email}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label for="password">Password</label>
+
+          <div className="col-sm-10">
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              placeholder="Choose a password"
+              value={member.password}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </fieldset>
     </form>
-    )
+  );
 }
-
-
-
-
