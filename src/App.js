74923a7309cf4d94f19144490864e91c46c05af2
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-09-18 16:15:36 -0500

    edit functionality in place. WIP tweaking styling

--- src/App.js
+++ src/App.js
@@ -23,17 +23,43 @@ function App() {
     };
     setMembers([...members, newMember]);
   }
+
+  const [edit, setEdit] = useState(false);
+  const initialEditState = {id: null, characterName:'',playerName:'', email:'',class:'',role:''};
+  const [currentMember, setCurrentMember] = useState(initialEditState);
   
   const memberToEdit = member => {
-    console.log(member)
+    setEdit(true)
+    setCurrentMember({id: member.id, characterName: member.characterName,playerName:member.playerName, email:member.email,class:member.class,role:member.role })
+  };
+
+  const updateMember = (id, updatedMember) => {
+    setEdit(false)
+    setMembers(members.map(member => (member.id === id ? updatedMember : member)))
   }
+
+
   return (
     <div className="App">
       <div className='member-list'>
         <h1>Party Members</h1>
-        <Members members={members} />
-        <MemberForm addNewMembers={addNewMembers}
-                    memberToEdit={memberToEdit} />
+        {edit ? ( 
+        <div>
+          <h2>Edit Group</h2>
+          <EditMember edit={edit}
+                        setEdit={setEdit}
+                        currentMember={currentMember}
+                        updateMember={updateMember}/>
+        </div>
+        ) : (
+          <div>
+            <h2>Add Party Members</h2>
+            <MemberForm addNewMembers={addNewMembers} />
+          </div>)}
+          
+
+              <Members members={members} memberToEdit={memberToEdit} />
+       
         
         
       </div>
