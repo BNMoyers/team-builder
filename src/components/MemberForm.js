9f9bdc9f07b60aac58b34d0cf876d73c66c6227b
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-09-18 15:25:44 -0500

    form for adding members complete

--- src/components/MemberForm.js
+++ src/components/MemberForm.js
@@ -1,46 +1,61 @@
 import React, { useState } from 'react';
 
 const MemberForm = props => {
+    console.log(props);
     const [member, setMember] = useState({ name:''});
 
+    const handleChange = event => {
+        setMember({...member, [event.target.name]: event.target.value })
+    };
+
+    const submitForm = event => {
+        event.preventDefault();
+        props.addNewMembers(member);
+        setMember({characterName: '',
+                   playerName: '',
+                   email: '',
+                   class: '',
+                   role: ''})
+    };
+
     return (
-        <form>
+        <form onSubmit={submitForm}>
             <label htmlFor='characterName'>Character Name:</label>
             <input type='text'
                     id='charname'
                     name='characterName'
                     value={member.characterName}
-    
+                    onChange={handleChange}
             />
              <label htmlFor='playerName'>Player Name:</label>
             <input type='text'
                     id='playername'
                     name='playerName'
                     value={member.playerName}
-    
+                    onChange={handleChange}
             />
              <label htmlFor='playerEmail'>Player Email:</label>
             <input type='email'
                     id='email'
                     name='email'
                     value={member.email}
-    
+                    onChange={handleChange}
             />
              <label htmlFor='class'>Character Class:</label>
             <input type='text'
                     id='class'
                     name='class'
                     value={member.class}
-    
+                    onChange={handleChange}
             />
               <label htmlFor='role'>Party Role:</label>
             <input type='text'
                     id='role'
                     name='role'
                     value={member.role}
-    
+                    onChange={handleChange}
             />
-            
+            <button type='submit'>Add Party Member</button>
     
         </form>
     )
